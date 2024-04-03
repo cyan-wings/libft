6992714f608ca1572733012e52691bafa272be37
Matthew Yeow <myeow@u80z04s05.42kl.edu.my>
2024-04-04 02:42:34 +0800

    Added ft_lstprint.c

--- srcs/lst_utils/ft_lstprint.c
+++ srcs/lst_utils/ft_lstprint.c
@@ -6,7 +6,7 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 20:03:02 by myeow             #+#    #+#             */
-/*   Updated: 2024/03/30 16:59:01 by myeow            ###   ########.fr       */
+/*   Updated: 2024/04/03 17:22:37 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 #include "../../includes/libft.h"
 #include "../../includes/ft_error.h"
 
-void	ft_lstprint(t_list *lst)
+void	ft_lstprint(t_list *lst, char sep)
 {
 	if (!lst)
 		ft_error(NULL_ERROR);
 	while (lst)
 	{
 		ft_putnbr_fd(*((int *) lst->content), 1);
-		ft_putchar_fd('\n', 1);
+		ft_putchar_fd(sep, 1);
 		lst = lst->next;
 	}
 }
