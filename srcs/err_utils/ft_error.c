b7a032bd2f8e3891f0cf20fce6b669c8d8820f11
Matthew Yeow <myeow@u80z04s05.42kl.edu.my>
2024-04-07 02:48:48 +0800

    Changes before push_swap submission

--- srcs/err_utils/ft_error.c
+++ srcs/err_utils/ft_error.c
@@ -6,18 +6,22 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 18:51:00 by myeow             #+#    #+#             */
-/*   Updated: 2024/04/01 17:32:07 by myeow            ###   ########.fr       */
+/*   Updated: 2024/04/07 01:36:45 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ft_error.h"
 #include "../../includes/libft.h"
 
+/*
+ * ft_putstr_fd("Error: ", 2);
+ * ft_putnbr_fd(e, 2);
+ * ft_putchar_fd('\n', 2);
+ */
 int	ft_error(t_error e)
 {
 	if (e == OK)
 		return (OK);
-	ft_putstr_fd("Error: ", 2);
-	ft_putnbr_fd(e, 2);
+	ft_putendl_fd("Error", 2);
 	return (e);
 }
--- srcs/str_utils/ft_free_ft_split.c
+++ srcs/str_utils/ft_free_ft_split.c
@@ -6,11 +6,12 @@
 /*   By: myeow <myeow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 18:59:36 by myeow             #+#    #+#             */
-/*   Updated: 2024/04/05 19:02:20 by myeow            ###   ########.fr       */
+/*   Updated: 2024/04/06 00:26:45 by myeow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 void	ft_free_ft_split(char **str_array)
 {
